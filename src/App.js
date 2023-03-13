e09620d3ef36e672044fa451839f881a414a0bff
Ntwali-Josue <kntwalijos@gmail.com>
2023-03-13 12:22:04 +0200

    Publisher components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Publishers from "./components/publishers/Publishers";
+import Articles from "./components/articles/Articles";
+
 function App() {
   return (
     <div className="text-center font-bold">
       <h1>News 10</h1>
+      <Publishers />
+      <Articles />
     </div>
   );
 }
